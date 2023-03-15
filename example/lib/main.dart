01e1c40705bf6ec8d6ad2fe0873b00b72098370c
Rodrigo Lago <rodrigo.lago@situm.com>
2023-03-15 17:10:49 +0100

    Load different buildings

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,275 +1,226 @@
+import 'package:flash/flash.dart';
 import 'package:flutter/material.dart';
-import 'package:situm_flutter_wayfinding/situm_flutter_sdk.dart';
 import 'package:situm_flutter_wayfinding/situm_flutter_wayfinding.dart';
 import 'package:situm_flutter_wayfinding_example/config.dart';
-import 'package:situm_flutter_wayfinding_example/find_my_car.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 const _title = "Situm Flutter Wayfinding";
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  String selectedBuildingIdentifier = buildingIdentifier;
+  SitumFlutterWayfinding? wayfinding;
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      routes: {
+        '/': (context) => HomeScreen(
+              selectedBuildingChangedCallback: _selectedBuildingChangedCallback,
+            ),
+        '/map': (context) => MapScreen(
+              situmMapLoaded: _onSitumMapLoaded,
+              selectedBuildingIdentifier: selectedBuildingIdentifier,
+              sizedBoxHeight: 10,
+            ),
+        '/otherMap': (context) => MapScreen(
+              situmMapLoaded: _onSitumMapLoaded,
+              selectedBuildingIdentifier: selectedBuildingIdentifier,
+              sizedBoxHeight: 50,
+            ),
+      },
       title: _title,
-      home: MyTabs(),
-    );
-  }
-}
-
-class MyTabs extends StatefulWidget {
-  const MyTabs({super.key});
-
-  @override
-  State<MyTabs> createState() => _MyTabsState();
-}
-
-class _MyTabsState extends State<MyTabs> {
-  late SitumFlutterSDK situmSdk;
-  int _selectedIndex = 0;
-  String currentOutput = "---";
-
-  SitumFlutterWayfinding? wyfController;
-
-  Widget _createHomeTab() {
-    // Home:
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'SitumSdk',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-              height: 150,
-              child: GridView.count(
-                  crossAxisCount: 4,
-                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  shrinkWrap: true,
-                  childAspectRatio: 1.5,
-                  children: [
-                    _sdkButton('Start', _requestUpdates),
-                    _sdkButton('Stop', _removeUpdates),
-                    _sdkButton('Prefetch', _prefetch),
-                    _sdkButton('Clear cache', _clearCache),
-                    _sdkButton('Pois', _fetchPois),
-                    _sdkButton('Categories', _fetchCategories),
-                    _sdkButton('Buildings', _fetchBuildings),
-                    _sdkButton('Building Info', _fetchBuildingInfo),
-                  ])),
-          Expanded(
-              child: SingleChildScrollView(
-                  padding:
-                      const EdgeInsets.all(30),
-                  child: Text(currentOutput)))
-        ],
-      ),
+      //home: MyTabs(),
     );
   }
 
-  Widget _sdkButton(String buttonText, void Function() onPressed) {
-    return TextButton(
-        onPressed: () {
-          onPressed();
-        },
-        child: Text(buttonText));
-  }
-
-  Widget _createSitumMapTab() {
-    // The Situm map:
-    return Stack(children: [
-      SitumMapView(
-        key: const Key("situm_map"),
-        // Your Situm credentials and building, see config.dart.
-        // Copy config.dart.example if you haven't already.
-        searchViewPlaceholder: "Situm Flutter Wayfinding",
-        situmUser: situmUser,
-        situmApiKey: situmApiKey,
-        buildingIdentifier: buildingIdentifier,
-        googleMapsApiKey: googleMapsApiKey,
-        useHybridComponents: true,
-        showPoiNames: true,
-        hasSearchView: true,
-        lockCameraToBuilding: true,
-        useRemoteConfig: true,
-        initialZoom: 20,
-        minZoom: 16,
-        maxZoom: 20,
-        showPositioningButton: true,
-        showNavigationIndications: true,
-        showFloorSelector: true,
-        navigationSettings: const NavigationSettings(
-          outsideRouteThreshold: 40,
-          distanceToGoalThreshold: 8,
-        ),
-        loadCallback: _onSitumMapLoaded,
-      ),
-      wyfController != null
-          ? FindMyCar(
-              wyfController: wyfController,
-              buildingIdentifier: buildingIdentifier,
-              selectedIconPath: 'resources/car_selected_icon.png',
-              unSelectedIconPath: 'resources/car_unselected_icon.png',
-            )
-          : Container()
-    ]);
+  void _selectedBuildingChangedCallback(String buildingId) {
+    if (selectedBuildingIdentifier != buildingId) {
+      wayfinding?.unload();
+    }
+    selectedBuildingIdentifier = buildingId;
   }
 
   void _onSitumMapLoaded(SitumFlutterWayfinding controller) {
-    // The Situm map was successfully loaded, use the given controller to
-    // call the WYF API methods.
-    print("WYF> Situm Map loaded!");
-    controller.onPoiSelected((poiSelectedResult) {
-      print("WYF> Poi ${poiSelectedResult.poiName} selected!");
-    });
-    controller.onPoiDeselected((poiDeselectedResult) {
-      print("WYF> Poi deselected!");
-    });
-    controller.onNavigationStarted((navigation) {
-      print("WYF> Nav started, distance = ${navigation.route?.distance}");
-    });
-
-    setState(() {
-      wyfController = controller;
-    });
+    wayfinding = controller;
   }
+}
+
+class HomeScreen extends StatefulWidget {
+  Function(String buildingId) selectedBuildingChangedCallback;
+
+  HomeScreen({required this.selectedBuildingChangedCallback});
 
   @override
-  void initState() {
-    // SitumSdk for flutter:
-    situmSdk = SitumFlutterSDK();
-    situmSdk.init(situmUser, situmApiKey);
-    situmSdk.setConfiguration(ConfigurationOptions(
-      useRemoteConfig: true,
-    ));
-    situmSdk.onEnterGeofences((geofencesResult) {
-      _echo("SDK> Enter geofences: ${geofencesResult.geofences}.");
-    });
-    situmSdk.onExitGeofences((geofencesResult) {
-      _echo("SDK> Exit geofences: ${geofencesResult.geofences}.");
-    });
-    super.initState();
-  }
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
 
-  void _echo(String output) {
-    setState(() {
-      currentOutput = output;
-      print(currentOutput);
-    });
-  }
-
-  void _requestUpdates() async {
-    situmSdk.requestLocationUpdates(
-      _MyLocationListener(echoer: _echo),
-      {"buildingIdentifier": buildingIdentifier},
-    );
-  }
-
-  void _removeUpdates() async {
-    situmSdk.removeUpdates();
-  }
-
-  void _clearCache() async {
-    _echo("SDK> RESPONSE: CLEAR CACHE...");
-    await situmSdk.clearCache();
-    _echo("SDK> RESPONSE: CLEAR CACHE = DONE");
-  }
-
-  void _prefetch() async {
-    _echo("SDK> PREFETCH...");
-    var prefetch = await situmSdk.prefetchPositioningInfo(
-      [buildingIdentifier],
-      options: PrefetchOptions(
-        preloadImages: true,
-      ),
-    );
-    _echo("SDK> RESPONSE: PREFETCH = $prefetch");
-  }
-
-  void _fetchPois() async {
-    _echo("SDK> POIS...");
-    var pois = await situmSdk.fetchPoisFromBuilding(buildingIdentifier);
-    _echo("SDK> RESPONSE: POIS = \n\n$pois");
-  }
-
-  void _fetchCategories() async {
-    _echo("SDK> CATEGORIES...");
-    var categories = await situmSdk.fetchPoiCategories();
-    _echo("SDK> RESPONSE: CATEGORIES = \n\n$categories");
-  }
-
-  void _fetchBuildingInfo() async {
-    _echo("SDK> BUILDING INFO...");
-    var building = await situmSdk.fetchBuildingInfo(buildingIdentifier);
-    _echo("SDK> RESPONSE: BUILDING INFO = \n\n$building)");
-  }
-
-  void _fetchBuildings() async {
-    _echo("SDK> BUILDINGS...");
-    var buildings = await situmSdk.fetchBuildings();
-    _echo("SDK> RESPONSE: BUILDINGS = \n\n$buildings");
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // The typical app widget with bottom navigation:
     return Scaffold(
       appBar: AppBar(
         title: const Text(_title),
       ),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: [_createHomeTab(), _createSitumMapTab()],
+      body: Card(
+        child: Column(
+          children: [
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  child: const Text('Go to map $buildingIdentifier!'),
+                  onPressed: () {
+                    setState(() {
+                      widget
+                          .selectedBuildingChangedCallback(buildingIdentifier);
+                      Navigator.of(context).pushReplacementNamed('/map');
+                    });
+                  },
+                ),
+                TextButton(
+                  child: const Text('Go to map $anotherBuildingIdentifier!'),
+                  onPressed: () {
+                    setState(() {
+                      widget.selectedBuildingChangedCallback(
+                          anotherBuildingIdentifier);
+                      Navigator.of(context).pushReplacementNamed('/otherMap');
+                    });
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  double sizedBoxHeight;
+  String selectedBuildingIdentifier;
+  Function(SitumFlutterWayfinding controller) situmMapLoaded;
+
+  MapScreen({
+    super.key,
+    required this.selectedBuildingIdentifier,
+    required this.sizedBoxHeight,
+    required this.situmMapLoaded,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  List<String> filters = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(_title),
+      ),
+      body: Column(
+        children: [
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed('/');
+                },
+                child: const Text('Go to Home!'),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (filters.isEmpty) {
+                      filters.add("1");
+                    } else {
+                      filters.clear();
+                    }
+                  });
+                },
+                child: const Text('Toggle coffee'),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Wayfinding',
+          Container(
+            height: widget.sizedBoxHeight,
+            color: Colors.black45,
+          ),
+          Expanded(
+            child: SitumMapView(
+              key: const Key("situm_map"),
+              // Your Situm credentials and building, see config.dart.
+              // Copy config.dart.example if you haven't already.
+              searchViewPlaceholder: "Situm Flutter Wayfinding",
+              situmUser: situmUser,
+              situmApiKey: situmApiKey,
+              buildingIdentifier: widget.selectedBuildingIdentifier,
+              googleMapsApiKey: googleMapsApiKey,
+              useHybridComponents: true,
+              showPoiNames: true,
+              hasSearchView: true,
+              lockCameraToBuilding: true,
+              useRemoteConfig: true,
+              initialZoom: 20,
+              showNavigationIndications: true,
+              showFloorSelector: true,
+              navigationSettings: const NavigationSettings(
+                outsideRouteThreshold: 40,
+                distanceToGoalThreshold: 8,
+              ),
+              loadCallback: _onSitumMapLoaded,
+              didUpdateCallback: _onSitumMapUpdated,
+            ),
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onSitumMapLoaded(SitumFlutterWayfinding controller) {
+    controller.onPoiSelected((poiSelectedResult) {
+      _showMessage("Selected ${poiSelectedResult.poiName}!!!");
     });
-  }
-}
-
-class _MyLocationListener implements LocationListener {
-  final Function echoer;
-
-  _MyLocationListener({required this.echoer});
-
-  @override
-  void onError(Error error) {
-    echoer("SDK> ERROR: ${error.message}");
+    widget.situmMapLoaded(controller);
   }
 
-  @override
-  void onLocationChanged(OnLocationChangedResult locationChangedResult) {
-    echoer(
-        "SDK> Location changed, building ID is: ${locationChangedResult.buildingId}");
+  void _onSitumMapUpdated(SitumFlutterWayfinding controller) {
+    controller.filterPoisBy(filters);
   }
 
-  @override
-  void onStatusChanged(String status) {
-    echoer("SDK> STATUS: $status");
+  void _showMessage(String message) {
+    if (!mounted) return;
+    print("Snack message: $message");
+    showFlash(
+      context: context,
+      duration: const Duration(seconds: 3),
+      builder: (_, controller) {
+        return Flash(
+          controller: controller,
+          position: FlashPosition.top,
+          behavior: FlashBehavior.fixed,
+          child: FlashBar(
+            icon: const Icon(
+              Icons.info,
+              size: 36.0,
+              color: Colors.black54,
+            ),
+            content: Text(message),
+          ),
+        );
+      },
+    );
   }
 }
