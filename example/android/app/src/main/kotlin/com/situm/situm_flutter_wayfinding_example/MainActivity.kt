87a1d78e04ea319d8d308355f13c343167e253e1
Rodrigo Lago <rodrigo.lago@situm.com>
2023-04-05 14:15:40 +0200

    Delete unused class

--- example/android/app/src/main/kotlin/com/situm/situm_flutter_wayfinding_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/situm/situm_flutter_wayfinding_example/MainActivity.kt
@@ -1,21 +1,5 @@
 package com.situm.situm_flutter_wayfinding_example
 
-import com.situm.situm_flutter_wayfinding.SitumMapFactory
-import io.flutter.embedding.android.FlutterAppCompatActivity
-import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : /*FlutterActivity()*/ FlutterAppCompatActivity() {
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        // Automatically register plugins:
-        super.configureFlutterEngine(flutterEngine)
-        // Reguster WYF widget:
-        flutterEngine
-            .platformViewsController
-            .registry
-            .registerViewFactory(
-                SitumMapFactory.CHANNEL_ID,
-                SitumMapFactory(flutterEngine.dartExecutor.binaryMessenger, this)
-            )
-    }
-}
+class MainActivity : FlutterFragmentActivity()
