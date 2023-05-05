4c73d4fe405f7594cf71a4f6aac255b00f2e2680
Rodrigo Lago <rodrigo.lago@situm.com>
2023-05-05 09:04:40 +0200

    Fix import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterWayfindingPlugin/config.dart';
+import './config.dart';
 import 'package:situm_flutter_wayfinding/situm_flutter_sdk.dart';
 import 'package:situm_flutter_wayfinding/situm_flutter_wayfinding.dart';
 
